fe26013c0cd4400099c3408d45c810673f5f739e
kirrog <kirrog@zertan.ru>
2022-12-08 22:48:01 +0300

    feat: load controller

---
+++ src/main/java/ru/itmo/demoproject/rest/RestControllerLoadDocument.java
@@ -0,0 +1,20 @@
+package ru.itmo.demoproject.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.itmo.demoproject.model.entity.dto.CopyRequestDTO;
+import ru.itmo.demoproject.model.entity.dto.LoadRequestDTO;
+import ru.itmo.demoproject.service.CopySevice;
+import ru.itmo.demoproject.service.LoaderService;
+
+@RestController
+@RequestMapping("load")
+@RequiredArgsConstructor
+public class RestControllerLoadDocument {
+    private final LoaderService service;
+    @PostMapping("")
+    public @ResponseBody ResponseEntity<ru.itmo.demoproject.model.entity.dto.ResponseBody> copyFileToUserDisk(@RequestBody LoadRequestDTO loadRequestDTO) {
+        return ResponseEntity.ok(service.loadDocument(loadRequestDTO));
+    }
+}
